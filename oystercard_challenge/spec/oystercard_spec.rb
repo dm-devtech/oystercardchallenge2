cda420c022e82d9257c56d18d136cf33da7ee2cb
Gonzalo Berhouet <gonzaloberhouet@Gonzalos-Air.fritz.box>
2020-10-05 16:01:59 +0100

    Point 5 Oystercard

--- oystercard_challenge/spec/oystercard_spec.rb
+++ oystercard_challenge/spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
-require 'oystercards'
-Describe Oystercard do
+require 'oystercard.rb'
+describe Oystercard do
+    it 'checks the balance in the card' do
+    expect{ subject.top_up 5}.to change{subject.balance }.by 5
+    end
+
+    # it 'checks the top up method' do
+    # expect(subject.top_up(5)).to eq 5
+    # end
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
 end

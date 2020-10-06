3f4463a6b69f665b65875ef0741cc4f6d2cfe9f0
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-06 15:37:03 +0100

    added in_journey? tests and method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,15 +3,21 @@ class Oystercard
 
     def initialize
         @balance = 0
+        @journey_status = false
     end
 
     def top_up(amount)
         fail 'You cannot have more than £90 in your balance' if @balance + amount > 90
         @balance += amount
     end
-    
+
     def deduct(deducted_am)
         @balance -= deducted_am
     end
+
+  def in_journey?
+    @journey_status
+  end
+
 
 end
